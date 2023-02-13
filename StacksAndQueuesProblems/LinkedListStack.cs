cb12052d80db0cd57e00ef5c7957730304cc99ee
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2023-02-13 15:55:09 +0530

    [Jyoti Rao] Create Stack

---
+++ StacksAndQueuesProblems/LinkedListStack.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StacksAndQueuesProblems
+{
+    public class LinkedListStack
+    {
+        Node top;
+        Node temp;
+        public void CreateStack()
+        {
+            Console.WriteLine("Enter number of node wanted to inserted");
+            int node=Convert.ToInt32(Console.ReadLine());
+            if(node==0)
+            {
+                return;
+            }
+            for(int i=1; i<=node; i++)
+            {
+                Console.Write("Enter the element to be inserted: ");
+                int data=Convert.ToInt32(Console.ReadLine());
+                Push(data);
+            }
+        }
+        public void Push( int data)
+        {
+            Node newNode = new Node(data);
+            if (top==null)
+            {
+                //Console.WriteLine("{0} is push into stack",data);
+                top = newNode;
+                return;
+            }
+            temp = top;
+            while (temp.next != null)
+            {
+                temp = temp.next;
+            }
+            temp.next = newNode;
+            //Console.WriteLine("{0} is push into stack", data);
+        }
+        public void DisplayStackList()
+        {
+            if (top == null)
+            {
+                Console.WriteLine("Stack is empty");
+                return;
+            }
+            else
+            {
+                Node temp = top;
+                Console.WriteLine("Element Present in Stack\n--------------------------");
+                while (temp != null)
+                {
+                    Console.Write(temp.data + "  ");
+                    temp = temp.next;
+                }
+            }
+        }
+    }
+}
---
+++ StacksAndQueuesProblems/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StacksAndQueuesProblems
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedListStack listStack = new LinkedListStack();
+            Console.WriteLine("Welcome to the Stack and Queues Problems");
+            bool check = true;
+            while (check)
+            {
+                Console.Clear();
+                Console.WriteLine("Please choose any options");
+                Console.WriteLine("1.CreateStack\n2.Push element into stack\n3.Display stack list\n10.Exit\n");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Console.Clear();
+                        listStack.CreateStack();
+                        Console.Write("\nPress any key to continue...... ");
+                        Console.ReadLine();
+                        break;
+                    case 2:
+                        Console.Clear();
+                        Console.Write("Enter the element to be push into stack: ");
+                        int data = Convert.ToInt32(Console.ReadLine());
+                        listStack.Push(data);
+                        Console.Write("\nPress any key to continue...... ");
+                        Console.ReadLine();
+                        break;
+                    case 3:
+                        Console.Clear();
+                        listStack.DisplayStackList();
+                        Console.Write("\nPress any key to continue...... ");
+                        Console.ReadLine();
+                        break;
+                    case 10:
+                        Console.Clear();
+                        break;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Select only valid options");
+                        Console.ReadLine();
+                        break;
+                }
+            }
+        }
+    }
+}
